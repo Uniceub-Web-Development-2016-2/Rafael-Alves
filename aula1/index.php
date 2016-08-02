fdba97b3fe2ad0c9d3e5753e6719ba3c9979d7f9
rafaelalvesceub <rafaelalvesfnde@gmail.com>
2016-08-02 19:09:51 -0300

    index alterado

--- aula1/index.php
+++ aula1/index.php
@@ -1,5 +1,13 @@
 <?php
-
-echo "Primeiro Arquivo"
-
+    echo $_POST['value'];
+$num = strlen($_POST['value']);
+if($num > 5){
+echo "Mais que 5 caracteres na string";
+}else{
+echo "Menos que 5 caracteres na string";
+}
 ?>
+<form method="post" action="">
+<input type="text" name="value">
+<input type="submit">
+</form>
